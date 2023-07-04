1c83a0cac8f1a7421a57e4f785e65a7a474921a8
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-07-04 17:34:43 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="header">
+      {/* <img src={logo} alt="logo" width={100} /> */}
+      <h1>Explore The World</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
