94e4c0f11dd54533a2087e31050a286f50f373c7
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-07-04 19:50:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import Country from "./components/Country";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { fetchCountries } from './redux/Country/countrySlice';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import CountryInfo from './components/CountryInfo';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <Country />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country/:countryCode" element={<CountryInfo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
