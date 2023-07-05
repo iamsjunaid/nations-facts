e3a9e3488f5e97fc9982dad71474f3b58d1d9cf3
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-07-05 15:38:16 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BiSolidMicrophone } from 'react-icons/bi';
 import { FiSettings } from 'react-icons/fi';
+import '../styles/Navbar.css';
+import logo from '../images/pngwing.com.png';
 
 function Navbar() {
   return (
     <nav className="header">
-      {/* <img src={logo} alt="logo" width={100} /> */}
-      <h1>Explore The World</h1>
+      <img src={logo} alt="logo" width={100} />
+      <h1>Nations-Facts</h1>
       <div className="icon">
         <BiSolidMicrophone className="icons" />
         <FiSettings className="icons" />
